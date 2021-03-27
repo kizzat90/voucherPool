eec0ed9481bbf8bb9e3206234ac79ed5e6150a14
Kamal Izzat <kamal@nplay.com.my>
2021-03-27 20:12:28 +0800

    Prevents newly created special offer to get hibernate error due to existing id.

--- src/main/java/com/boost/voucherpool/service/SpecialOfferService.java
+++ src/main/java/com/boost/voucherpool/service/SpecialOfferService.java
@@ -14,6 +14,7 @@ public class SpecialOfferService {
 
     // Create new Special Offer
     public SpecialOffer newSpecialOffer(SpecialOffer specialOffer) {
+        specialOffer.setSpecialOfferId(null);
         return specialOfferRepository.save(specialOffer);
     }
 
